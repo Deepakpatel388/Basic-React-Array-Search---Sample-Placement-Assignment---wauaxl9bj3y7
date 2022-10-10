53216b5c11c382d275b01bd3c3a31a1699f471a6
Deepakpatel388 <deepakanand388@gmail.com>
2022-10-10 12:03:16 +0530

    fin

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,29 @@
-import React, { useState } from "react";
-import Value from "./Value"
-import "../styles/App.css";
+import React from 'react'
+import { useState } from 'react'
 
-const searchArray = [
-  "Newton",
-  "School",
-  "Newton School",
-  "React",
-  "Preact",
-  "Node",
-  "Mongoose",
-  "Angular",
-  "Vue",
-  "Apple",
-  "Microsoft",
-  "Netflix",
-  "Meta",
-];
+import '../styles/App.css';
+
+
+const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
-  let inputVal = "";
-  let [searchedArr, setSearchedArr] = useState([]);
-
-  const valueOfInput = (event) => {
-    setSearchedArr([]);
-   inputVal = event.target.value;
-    setTimeout(() => {
-      if(inputVal !== ""){
-        displayResult(inputVal);
-      }
-    }, 500);
-    
-  };
-
-  const displayResult = (inputVal) => {
-    
-    let arr = [];
-    searchArray.map((str) => {
-      str = str.toLowerCase();
-      let substr = inputVal.toLowerCase();
-      console.log(substr,"line34")
-      if (str.indexOf(substr) !== -1) {
-        console.log(str);
-        arr.push(str);
-      }
-    });
-    setSearchedArr(arr);
-    // let ans = searchedArr.map((item) => {
-    //   return <list value={item} />;
-    // });
-  };
-
+   const [text, setText] = useState('');
+   const handleInput =(e)=>{
+      const input = e.target.value;
+      setText(input);
+   }
+  
   return (
     <div id="main">
-      <h1>Search</h1>
-      <input type={"text"} onChange={valueOfInput} />
-      <h3>Results</h3>
-      <ol id="ul">
-        {                  
-          searchedArr.map((item,index) => {
-          return <Value key={index} value={item} />;
-        })}
-      </ol>
+      <input type = "text" id = "search-input" placeholder = "search..." onChange = {handleInput}/>
+        {searchArray.filter(val=>val.toLowerCase().includes(text))                      
+            .map((val)=>(             
+                <div className = "user">
+                    <li>{val}</li>
+                </div>           
+        ))}
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
